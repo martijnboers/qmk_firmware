729603059f4bc9cdca38c4fa56a4b0c863cdfe5f
Martijn Boers <martijn@plebian.nl>
2019-02-25 23:47:46 +0100

    Add EEROM reset button

--- keyboards/preonic/keymaps/martijn/config.h
+++ keyboards/preonic/keymaps/martijn/config.h
@@ -4,16 +4,13 @@
 #include "config_common.h"
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PREONIC_SOUND)
-    // #define STARTUP_SONG SONG(NO_SOUND)
+    //#define STARTUP_SONG SONG(PREONIC_SOUND)
+    #define STARTUP_SONG SONG(NO_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
-                                }
 #endif
 
 #define MUSIC_MASK (keycode != KC_NO)
+#define SELECT    LCTL(KC_LSFT)
 
 /*
  * MIDI options
