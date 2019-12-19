2803b6d57ade5db97a2bcc8a6b90612092fd786d
Martijn Boers <martijn@plebian.nl>
2019-12-19 11:37:35 +0100

    extra extra key

--- keyboards/preonic/keymaps/martijn/keymap.c
+++ keyboards/preonic/keymaps/martijn/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   POK,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
-  KC_LCTL,  KC_LGUI, EXTR,    KC_LALT,          PROG,    KC_SPC,           KC_LGUI, OKE,     KC_PSCR, KC_LCTL
+  KC_LCTL,  EXTR,    EXTR,    KC_LALT,          PROG,    KC_SPC,           KC_LGUI, OKE,     KC_PSCR, KC_LCTL
 ),
 
 
@@ -75,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_EXTRA] = LAYOUT_preonic_2x2u( \
-  XXXXXXX,   RESET,     EEP_RST,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MINS, KC_EQL,  XXXXXXX, XXXXXXX, \
-  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_7,    KC_8,    KC_9,    XXXXXXX, \
-  XXXXXXX,   XXXXXXX,   MU_TOG,    XXXXXXX,   XXXXXXX,   XXXXXXX,   HEADP,     XXXXXXX,   KC_4,    KC_5,    KC_6,    KC_DEL, \
-  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   SONG,      KC_1,    KC_2,    KC_3,    XXXXXXX, \
-  XXXXXXX,   XXXXXXX,   KC_TRNS,   XXXXXXX,              XXXXXXX,   KC_SPC,               KC_0,    KC_0,    KC_0,    XXXXXXX
+  XXXXXXX,   RESET,     EEP_RST,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MINS, KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_7,    KC_8,    KC_9,    XXXXXXX, XXXXXXX, \
+  XXXXXXX,   XXXXXXX,   MU_TOG,    XXXXXXX,   XXXXXXX,   XXXXXXX,   HEADP,     KC_4,    KC_5,    KC_6,    KC_DEL,  XXXXXXX, \
+  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   SONG,      KC_1,    KC_2,    KC_3,    XXXXXXX, XXXXXXX, \
+  XXXXXXX,   XXXXXXX,   KC_TRNS,   XXXXXXX,              XXXXXXX,   KC_SPC,             KC_0,    KC_0,    KC_0,    XXXXXXX
 )
 
 };
