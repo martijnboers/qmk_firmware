d1c032827550f2bbd46a45047fd5960ffbd7a367
Martijn Boers <martijn@plebian.nl>
2023-03-28 18:34:40 +0200

    ported preonic to lumberjack

---
+++ keyboards/peej/lumberjack/keymaps/martijnboers/config.h
@@ -0,0 +1,31 @@
+#define TAPPING_TERM 200
+
+#define COPY      LCTL(KC_C)
+#define PASTE     LCTL(KC_V)
+#define T_PREV    LCTL(KC_PGUP)
+#define T_NEXT    LCTL(KC_PGDN)
+#define N_TAB     LCTL(KC_T)
+#define W_QUIT    LALT(KC_F4)
+
+#define ALL       LCTL(KC_A)
+
+#define E_BACK     S(A(KC_LEFT))
+#define E_FORWARD  S(A(KC_RIGHT))
+
+#define SWITCH    LALT(KC_ESC)
+#define TERMINAL  RGUI(KC_T)
+
+#define OSUP      LCTL(LALT(KC_UP))
+#define OSDOWN    LCTL(LALT(KC_DOWN))
+
+#define PUSH_2_TALK LCTL(KC_F23)
+
+#define MOUSEKEY_INTERVAL           20
+#define MOUSEKEY_DELAY              100
+#define MOUSEKEY_TIME_TO_MAX        60
+#define MOUSEKEY_MAX_SPEED          7
+#define MOUSEKEY_WHEEL_MAX_SPEED    5
+#define MOUSEKEY_WHEEL_TIME_TO_MAX  60
+#define MOUSEKEY_WHEEL_DELAY        100
+
+#define UNICODE_SELECTED_MODES UC_LNX
--- keyboards/peej/lumberjack/rules.mk
+++ keyboards/peej/lumberjack/rules.mk
@@ -10,3 +10,7 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+
+TAP_DANCE_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+COMMAND_ENABLE = no
