13cd640e140261e4ca593f07ad31a3f9a872099d
Martijn Boers <martijn@plebian.nl>
2023-03-28 23:12:52 +0200

    cleanup rules

--- keyboards/peej/lumberjack/rules.mk
+++ keyboards/peej/lumberjack/rules.mk
@@ -1,8 +1,8 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -12,6 +12,5 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
 TAP_DANCE_ENABLE = yes
-BOOTMAGIC_ENABLE = yes
 COMMAND_ENABLE = no
 UNICODEMAP_ENABLE = yes
