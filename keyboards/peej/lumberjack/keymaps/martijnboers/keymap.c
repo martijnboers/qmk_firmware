4b72c733c329643d6440f97820b293885f9e34ac
Martijn Boers <martijn@plebian.nl>
2023-03-28 23:11:42 +0200

    unicode passive aggressiveness

--- keyboards/peej/lumberjack/keymaps/martijnboers/keymap.c
+++ keyboards/peej/lumberjack/keymaps/martijnboers/keymap.c
@@ -70,8 +70,11 @@ tap_dance_action_t tap_dance_actions[] = {
 
 enum custom_keycodes {
   HEADP = SAFE_RANGE,
-  C_THUMB,
   C_HOME,
+};
+
+const uint32_t unicode_map[] PROGMEM = {
+    [0]  = 0x1F44D,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -86,15 +89,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ctrl |  os  | mod3  | alt  |  program   |    space    | os   | $    | print| ctrl |
+ * | ctrl |  os  | mod3  | alt  |  program   |    space    | os   | duim | print| ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_ortho_5x12( \
-  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS, \
-  POKER,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT, \
-  SC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, SC_RSPC, \
-  KC_LCTL,  EXTR,    EXTR,    KC_LALT, PROG,    PROG,    KC_SPC,  KC_SPC,  KC_LGUI, C_THUMB,KC_PSCR, KC_LCTL
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,       KC_0,    KC_BSPC, \
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,       KC_P,    KC_BSLS, \
+  POKER,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,       KC_SCLN, KC_ENT, \
+  SC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,     KC_SLSH, SC_RSPC, \
+  KC_LCTL,  EXTR,    EXTR,    KC_LALT, PROG,    PROG,    KC_SPC,  KC_SPC,  KC_LGUI, X(0),       KC_PSCR, KC_LCTL
 ),
 
 
@@ -145,7 +148,7 @@ void keyboard_post_init_user() {
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if(res) {
-        writePin(LED2, !led_state.caps_lock);
+        writePin(LED2, led_state.caps_lock);
     }
     return res;
 }
@@ -164,13 +167,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case C_HOME:
       if (record->event.pressed) {
         SEND_STRING("~/");
-      }
-
-      break;
-
-    case C_THUMB:
-      if (record->event.pressed) {
-        SEND_STRING(":+1:");
       }
 
       break;
--- keyboards/peej/lumberjack/rules.mk
+++ keyboards/peej/lumberjack/rules.mk
@@ -14,3 +14,4 @@ AUDIO_ENABLE = no           # Audio output
 TAP_DANCE_ENABLE = yes
 BOOTMAGIC_ENABLE = yes
 COMMAND_ENABLE = no
+UNICODEMAP_ENABLE = yes
