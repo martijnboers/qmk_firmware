49d4dc7fa3c72887c889dffe1b1d3599e8944df2
Martijn Boers <martijn@boers.email>
2025-11-05 22:28:54 +0100

    Change out duim with tapdance []
    Attempt at fixing quickfix etc navigation neovim

--- keyboards/peej/lumberjack/keymaps/martijn/keymap.c
+++ keyboards/peej/lumberjack/keymaps/martijn/keymap.c
@@ -45,7 +45,8 @@ void altlp_reset(tap_dance_state_t *state, void *user_data);
 
 // Tap Dance keycodes
 enum td_keycodes {
-    TAP_SHFT
+    TAP_SHFT,
+    TD_BRACKETS // <-- ADDED: A name for our new tap dance
 };
 
 enum custom_keycodes {
@@ -53,13 +54,7 @@ enum custom_keycodes {
   C_HOME,
 };
 
-enum unicode_names {
-    THUMB,
-};
-
-const uint32_t PROGMEM unicode_map[] = {
-    [THUMB]  = 0x1F44D, // 🐍
-};
+// <-- REMOVED: The `enum unicode_names` and `unicode_map[]` are no longer needed.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -81,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,       KC_P,    KC_BSLS, \
   POKER,    KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,       KC_SCLN, KC_ENT, \
   SC_LSPO,  KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,     KC_SLSH, SC_RSPC, \
-  KC_LCTL,  EXTR,    KC_LGUI,    KC_LALT, PROG,    PROG,    KC_SPC,  KC_SPC,  KC_RALT, UM(THUMB),  KC_PSCR, KC_LCTL
+  KC_LCTL,  EXTR,    KC_LGUI,    KC_LALT, PROG,    PROG,    KC_SPC,  KC_SPC,  KC_RALT, TD(TD_BRACKETS),  KC_PSCR, KC_LCTL
 ),
 
 
@@ -177,8 +172,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 };
 
+void brackets_tap_dance_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        // Singl tap = ]
+        register_code16(KC_RBRC);
+    } else if (state->count == 2) {
+        // Double tap = [
+        register_code16(KC_LBRC);
+    }
+}
 
-// Determine the tapdance state to return
+void brackets_tap_dance_reset(tap_dance_state_t *state, void *user_data) {
+    // Unregister keys on reset
+    unregister_code16(KC_LBRC);
+    unregister_code16(KC_RBRC);
+}
+
+
 td_state_t cur_dance(tap_dance_state_t *state) {
     if (state->count == 1) {
         if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
@@ -230,5 +240,6 @@ void tap_reset(tap_dance_state_t *state, void *user_data) {
 
 // Define `ACTION_TAP_DANCE_FN_ADVANCED()` for each tapdance keycode, passing in `finished` and `reset` functions
 tap_dance_action_t tap_dance_actions[] = {
-    [TAP_SHFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tap_finished, tap_reset)
+    [TAP_SHFT]   = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tap_finished, tap_reset),
+    [TD_BRACKETS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, brackets_tap_dance_finished, brackets_tap_dance_reset) // <-- ADDED
 };
